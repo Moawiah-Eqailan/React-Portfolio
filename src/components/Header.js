0df75ab1d5eca77db2558025681ed64e05ae5a93
Moawiah-Eqailan <meqailan5@gmail.com>
2024-12-15 15:37:59 +0300

    fixing bugs

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,25 +7,25 @@ function Header({ isDarkMode, toggleDarkMode }) {
 
   return (
     <header className={isDarkMode ? 'dark-mode' : ''}>
-      <a href="#" className="logo">Moawiah Eqailan</a> 
+      <a href="/" className="logo">Moawiah Eqailan</a> 
       
       <button onClick={toggleDarkMode} className="dark-mode-toggle">
         <i className={isDarkMode ? 'fa fa-sun' : 'fa fa-moon'}></i>
       </button>
 
       <nav className="navigation">
-        <a href="#">
+        <a href="/">
       <ScrollLink to="Services" >Services</ScrollLink></a>
-      <a href="#">
+      <a href="/">
         <ScrollLink to="Skills" >Skills</ScrollLink>
       </a>
-        <a href="#">
+        <a href="/">
          <ScrollLink to="Projects" >Projects</ScrollLink>
        </a>
-        <a href="#">
+        <a href="/">
         <ScrollLink to="Testimonials" >Testimonials</ScrollLink>
         </a>
-        <a href="#">
+        <a href="/">
         <ScrollLink to="Highlight" >Highlight</ScrollLink>
         </a>
         <Link to="/Contact">Contact</Link>
